bfdb811269d6ae365dbf4487f48a102ffbd0258b
tomsherm <tomsherm@umich.edu>
2022-07-02 23:44:29 -0500

    GeoReader menu improvements. More improvements coming for the IWL screen.

--- InfiniteWordLadder/Menu.swift
+++ InfiniteWordLadder/Menu.swift
@@ -14,158 +14,123 @@ struct Menu: View {
     @State private var opacity = 1.0
     @State private var scale = 1.0
     
-    var deviceWidth: CGFloat {
-        UIScreen.main.bounds.width
-    }
-
-    var deviceHeight: CGFloat {
-        UIScreen.main.bounds.height
-    }
-    
-    
     var body: some View {
         ZStack{
-            Color(red:0.0, green:0.5, blue:1.0)
-            ZStack{
-            HStack{
-                Rectangle()
-                   .fill(.white)
-                   .frame(width: deviceWidth/8, height: deviceHeight/1.5)
-                   .cornerRadius(20)
-                   .padding(.trailing)
-                   .padding(.trailing)
-                Rectangle()
-                   .fill(.white)
-                   .frame(width: deviceWidth/8, height: deviceHeight/1.5)
-                   .cornerRadius(20)
-                   .padding(.leading)
-                   .padding(.leading)
+            Color.blue.ignoresSafeArea()
+            GeometryReader{ geo in
+                ZStack{
+                    HStack(spacing: 0) {
+                        Rectangle()
+                            .foregroundColor(.blue)
+                            .frame(width: geo.size.width * 0.20)
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .frame(width: geo.size.width * 0.20, height: geo.size.height*0.875)
+                            .cornerRadius(30)
+                        Rectangle()
+                            .foregroundColor(.blue)
+                            .frame(width: geo.size.width * 0.20)
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .frame(width: geo.size.width * 0.20, height: geo.size.height*0.875)
+                            .cornerRadius(30)
+                        Rectangle()
+                            .foregroundColor(.blue)
+                            .frame(width: geo.size.width * 0.20)
+                    }
+                    
+                    VStack{
+                        Button("Infinite Word Ladder"){
+                            //withAnimation{
+                            //    angle += 360.0
+                            //}
+                            //For rotation
+                            opacity -= 1
+                            scale -= 1
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                gameinfo.currPage = .IWL
+                            }}
+                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                        .padding()
+                        .frame(width: geo.size.width*0.85, height: geo.size.height*0.10)
+                        .background(Color.white)
+                        .cornerRadius(40)
+                        .foregroundColor(Color.blue)
+                        .padding()
+                        Button("Instructions"){
+                            angle += 360.0
+                            opacity -= 1
+                            scale -= 1
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                gameinfo.currPage = .instructions
+                            }
+                        }
+                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                        .padding()
+                        .frame(width: geo.size.width*0.85, height: geo.size.height*0.10)
+                        .background(Color.white)
+                        .cornerRadius(40)
+                        .foregroundColor(Color.blue)
+                        .padding()
+                        Button("Statistics"){
+                            angle += 360.0
+                            opacity -= 1
+                            scale -= 1
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                gameinfo.currPage = .stats
+                            }
+                        }
+                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                        .padding()
+                        .frame(width: geo.size.width*0.85, height: geo.size.height*0.10)
+                        .background(Color.white)
+                        .cornerRadius(40)
+                        .foregroundColor(Color.blue)
+                        .padding()
+                        Button("Achievements"){
+                            angle += 360.0
+                            opacity -= 1
+                            scale -= 1
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                gameinfo.currPage = .achievements
+                            }
+                        }
+                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                        .padding()
+                        .frame(width: geo.size.width*0.85, height: geo.size.height*0.10)
+                        .background(Color.white)
+                        .cornerRadius(40)
+                        .foregroundColor(Color.blue)
+                        .padding()
+                        Button("Settings"){
+                            angle += 360.0
+                            opacity -= 1
+                            scale -= 1
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                gameinfo.currPage = .settings
+                            }
+                        }
+                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                        .padding()
+                        .frame(width: geo.size.width*0.85, height: geo.size.height*0.10)
+                        .background(Color.white)
+                        .cornerRadius(40)
+                        .foregroundColor(Color.blue)
+                        .padding()
+                    }
+                }
             }
-            VStack{
-                Button("Infinite Word Ladder"){
-                    //withAnimation{
-                    //    angle += 360.0
-                    //}
-                    //For rotation
-                    opacity -= 1
-                    scale -= 1
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                        gameinfo.currPage = .IWL
-                    }
-                }
-                .font(.system(size: 20, weight: .bold, design: .rounded))
-                .padding()
-                .frame(width: deviceWidth/1.5, alignment: .center)
-                .background(Color.white)
-                .cornerRadius(40)
-                .foregroundColor(Color.blue)
-                .padding()
-//                .rotationEffect(.degrees(angle))
-//                .animation(.easeInOut, value: angle)
-//                .opacity(opacity)
-//                .animation(.easeInOut, value: opacity)
-//                .scaleEffect(scale)
-//                .animation(.easeIn, value: scale)
-                
-                Button("Instructions"){
-                    angle += 360.0
-                    opacity -= 1
-                    scale -= 1
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                        gameinfo.currPage = .instructions
-                    }
-                }
-                .font(.system(size: 20, weight: .bold, design: .rounded))
-                .padding()
-                .frame(width: deviceWidth/1.5, alignment: .center)
-                .background(Color.white)
-                .cornerRadius(40)
-                .foregroundColor(Color.blue)
-                .padding()
-//                .rotationEffect(.degrees(angle))
-//                .animation(.easeInOut, value: angle)
-//                .opacity(opacity)
-//                .animation(.easeInOut, value: opacity)
-//                .scaleEffect(scale)
-//                .animation(.easeIn, value: scale)
-                
-                Button("Statistics"){
-                    angle += 360.0
-                    opacity -= 1
-                    scale -= 1
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                        gameinfo.currPage = .stats
-                    }
-                }
-                .font(.system(size: 20, weight: .bold, design: .rounded))
-                .padding()
-                .frame(width: deviceWidth/1.5, alignment: .center)
-                .background(Color.white)
-                .cornerRadius(40)
-                .foregroundColor(Color.blue)
-                .padding()
-//                .rotationEffect(.degrees(angle))
-//                .animation(.easeInOut, value: angle)
-//                .opacity(opacity)
-//                .animation(.easeInOut, value: opacity)
-//                .scaleEffect(scale)
-//                .animation(.easeIn, value: scale)
-                
-                Button("Acheivements"){
-                    angle += 360.0
-                    opacity -= 1
-                    scale -= 1
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                        gameinfo.currPage = .achievements
-                    }
-                }
-                .font(.system(size: 20, weight: .bold, design: .rounded))
-                .padding()
-                .frame(width: deviceWidth/1.5, alignment: .center)
-                .background(Color.white)
-                .cornerRadius(40)
-                .foregroundColor(Color.blue)
-                .padding()
-//                .rotationEffect(.degrees(angle))
-//                .animation(.easeInOut, value: angle)
-//                .opacity(opacity)
-//                .animation(.easeInOut, value: opacity)
-//                .scaleEffect(scale)
-//                .animation(.easeIn, value: scale)
-                Button("Settings"){
-                    angle += 360.0
-                    opacity -= 1
-                    scale -= 1
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                        gameinfo.currPage = .settings
-                    }
-                }
-                .font(.system(size: 20, weight: .bold, design: .rounded))
-                .padding()
-                .frame(width: deviceWidth/1.5, alignment: .center)
-                .background(Color.white)
-                .cornerRadius(40)
-                .foregroundColor(Color.blue)
-                .padding()
-//                .rotationEffect(.degrees(angle))
-//                .animation(.easeInOut, value: angle)
-//                .opacity(opacity)
-//                .animation(.easeInOut, value: opacity)
-//                .scaleEffect(scale)
-//                .animation(.easeIn, value: scale)
-            }
-            //.rotationEffect(.degrees(angle))
-            //.animation(.easeInOut, value: angle)
+            .opacity(opacity)
+            .animation(.easeOut, value: opacity)
+            .scaleEffect(scale)
+            .animation(.easeOut, value: scale)
+            .rotationEffect(.degrees(angle))
         }
-        .opacity(opacity)
-        .animation(.easeOut, value: opacity)
-        .scaleEffect(scale)
-        .animation(.easeOut, value: scale)
-        .rotationEffect(.degrees(angle))
-        }
-        .ignoresSafeArea()
     }
 }
+    
+    
+    
 
 struct Menu_Previews: PreviewProvider {
     static var previews: some View {
@@ -173,3 +138,123 @@ struct Menu_Previews: PreviewProvider {
             .environmentObject(GameInfo())
     }
 }
+
+
+
+/*var deviceWidth: CGFloat {
+ UIScreen.main.bounds.width
+}
+
+var deviceHeight: CGFloat {
+ UIScreen.main.bounds.height
+}
+
+
+var body: some View {
+ ZStack{
+     Color(red:0.0, green:0.5, blue:1.0)
+     ZStack{
+     HStack{
+         Rectangle()
+            .fill(.white)
+            .frame(width: deviceWidth/8, height: deviceHeight/1.5)
+            .cornerRadius(20)
+            .padding(.trailing)
+            .padding(.trailing)
+         Rectangle()
+            .fill(.white)
+            .frame(width: deviceWidth/8, height: deviceHeight/1.5)
+            .cornerRadius(20)
+            .padding(.leading)
+            .padding(.leading)
+     }
+     VStack{
+         Button("Infinite Word Ladder"){
+             //withAnimation{
+             //    angle += 360.0
+             //}
+             //For rotation
+             opacity -= 1
+             scale -= 1
+             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                 gameinfo.currPage = .IWL
+             }
+         }
+         .font(.system(size: 20, weight: .bold, design: .rounded))
+         .padding()
+         .frame(width: deviceWidth/1.5, alignment: .center)
+         .background(Color.white)
+         .cornerRadius(40)
+         .foregroundColor(Color.blue)
+         .padding()
+         Button("Instructions"){
+             angle += 360.0
+             opacity -= 1
+             scale -= 1
+             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                 gameinfo.currPage = .instructions
+             }
+         }
+         .font(.system(size: 20, weight: .bold, design: .rounded))
+         .padding()
+         .frame(width: deviceWidth/1.5, alignment: .center)
+         .background(Color.white)
+         .cornerRadius(40)
+         .foregroundColor(Color.blue)
+         .padding()
+         Button("Statistics"){
+             angle += 360.0
+             opacity -= 1
+             scale -= 1
+             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                 gameinfo.currPage = .stats
+             }
+         }
+         .font(.system(size: 20, weight: .bold, design: .rounded))
+         .padding()
+         .frame(width: deviceWidth/1.5, alignment: .center)
+         .background(Color.white)
+         .cornerRadius(40)
+         .foregroundColor(Color.blue)
+         .padding()
+         Button("Acheivements"){
+             angle += 360.0
+             opacity -= 1
+             scale -= 1
+             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                 gameinfo.currPage = .achievements
+             }
+         }
+         .font(.system(size: 20, weight: .bold, design: .rounded))
+         .padding()
+         .frame(width: deviceWidth/1.5, alignment: .center)
+         .background(Color.white)
+         .cornerRadius(40)
+         .foregroundColor(Color.blue)
+         .padding()
+         Button("Settings"){
+             angle += 360.0
+             opacity -= 1
+             scale -= 1
+             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                 gameinfo.currPage = .settings
+             }
+         }
+         .font(.system(size: 20, weight: .bold, design: .rounded))
+         .padding()
+         .frame(width: deviceWidth/1.5, alignment: .center)
+         .background(Color.white)
+         .cornerRadius(40)
+         .foregroundColor(Color.blue)
+         .padding()
+     }
+ }
+ .opacity(opacity)
+ .animation(.easeOut, value: opacity)
+ .scaleEffect(scale)
+ .animation(.easeOut, value: scale)
+ .rotationEffect(.degrees(angle))
+ }
+ .ignoresSafeArea()
+}
+}*/
